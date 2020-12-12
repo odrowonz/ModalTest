c797e4eeffb615d1a3ff0cb4edbec031a8cffc90
Andrey Antipov <odrowonz@gmail.com>
2020-12-12 22:00:32 +0300

    Улучшение стиля.

--- ModalTest/ViewController.swift
+++ ModalTest/ViewController.swift
@@ -8,28 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "openModalButtonPressed" {
-            if let controller = segue.destination as? ModalViewController {
-                guard let nav = self.navigationController,
-                      let root = nav.viewControllers[0] as? RootViewController
-                else { return }
+            guard let controller = segue.destination as? ModalViewController,
+                  let nav = self.navigationController,
+                  let root = nav.viewControllers[0] as? RootViewController
+                  else { return }
 
-                root.eventLabel?.text = "Открываем модально!"
+            root.eventLabel?.text = "Открываем модально!"
 
-                controller.submitFinalAction = {
-                    root.eventLabel?.text = "Ура, прошли полный цикл!"
-
-                    nav.popToRootViewController(animated: true)
-                }
+            controller.submitFinalAction = {
+                root.eventLabel?.text = "Ура, прошли полный цикл!"
+                nav.popToRootViewController(animated: true)
             }
         }
     }
-
 }
